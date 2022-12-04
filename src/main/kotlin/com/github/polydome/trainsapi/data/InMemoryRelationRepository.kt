1d25e9c4bb6df4ece635fed9b6ed7a9a040825ce
polydome <polydome@outlook.com>
2022-12-04 16:35:46 +0100

    [relations] implement mock relation repository

---
+++ src/main/kotlin/com/github/polydome/trainsapi/data/InMemoryRelationRepository.kt
@@ -0,0 +1,22 @@
+package com.github.polydome.trainsapi.data
+
+import com.github.polydome.trainsapi.model.Relation
+import com.github.polydome.trainsapi.model.RelationId
+import com.github.polydome.trainsapi.repository.RelationRepository
+
+class InMemoryRelationRepository : RelationRepository {
+    private val relations = mutableListOf<Relation>()
+
+    override fun createRelation(name: String) {
+        val id = RelationId(name.hashCode().toString())
+        val relation = Relation(
+            name = name,
+            id = id
+        )
+        relations.add(relation)
+    }
+
+    override fun findRelation(id: RelationId): Relation? {
+        return relations.find { it.id == id }
+    }
+}
--- src/main/kotlin/com/github/polydome/trainsapi/repository/RelationRepository.kt
+++ src/main/kotlin/com/github/polydome/trainsapi/repository/RelationRepository.kt
@@ -1,8 +1,9 @@
 package com.github.polydome.trainsapi.repository
 
+import com.github.polydome.trainsapi.model.Relation
 import com.github.polydome.trainsapi.model.RelationId
 
 interface RelationRepository {
     fun createRelation(name: String)
-    fun findRelation(id: RelationId): RelationId
+    fun findRelation(id: RelationId): Relation?
 }
